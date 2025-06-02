77d9808f4eeb238eaa85103cd609c5717f79be0b
Ulhas <ch.sai.ulhas@gmail.com>
2025-06-02 08:31:55 +0530

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import DoctorLandingPage from './DoctorLandingPage'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DoctorLandingPage />
+  </React.StrictMode>
+)
